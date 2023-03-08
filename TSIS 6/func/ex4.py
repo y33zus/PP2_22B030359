ea7dd6fd2ab46f870c5f8d5da0e1ea16f3573234
y33zus <z_zholdasbay@kbtu.kz>
2023-03-08 20:44:16 +0600

    0

--- TSIS 6/func/ex4.py
+++ TSIS 6/func/ex4.py
@@ -1,5 +1,5 @@
 def all_true(my_tuple):
     return all(my_tuple)
-tuple1 = (True, True, True)
-tuple2 = (True, False, True)
+tuple1 = (True, True, True,True,True)
+tuple2 = (True, False, True,False,False)
 print(all_true(tuple1),all_true(tuple2))
